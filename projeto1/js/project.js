6038b461c3c64f3502230a651a1a464418e25580
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-10-04 16:48:22 +0100

    lower and upper mobiles rotating

--- projeto1/js/project.js
+++ projeto1/js/project.js
@@ -2,9 +2,12 @@ var horizontal = true, vertical = false;
 var camera, scene, renderer;
 var geometry, material, mesh, cube;
 var lowerMobile, middleMobile, upperMobile, mobile;
-var scale = 10;
+var scale = 6;
 var stdRadius = 2 / scale;
 var cylinderHeight = 4, cylinderRadius = 2;
+var rotate_v1_r, rotate_v1_l, rotate_v2_r, rotate_v2_l, rotate_v3_r, rotate_v3_l;
+var xs, ys, radius, heights, orientations;
+var leftArrow, topArrow, rightArrow, downArrow;
 
 function addCylinder(obj, x, y, z, r1, r2, height, horizontal) {
     'use strict';
@@ -31,15 +34,30 @@ function addCube(obj, x, y, z, a, diagonal) {
 function createLowerMobile(x, y, z) {
     'use strict';
     lowerMobile = new THREE.Object3D();
+    /*
     lowerMobile.userData = { 
         rotateX: 0, 
         rotateY: 0,
         rotateZ: 0,
         step: 0.05
     };
-
+    */
     material = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: true });
+    xs = [0, -3, 2, -8, -8, 6, -2, 14, 19, 10, 28, 9, 4, 14, 4, -2, 28, 14];
+    ys = [-2, -4, -8, -6, -10, -12, -14, -14, -16, -18, -18, -20, -23, -22, -28, -17, -21, -25];
+    radius = [stdRadius, stdRadius, stdRadius, stdRadius, cylinderRadius, stdRadius, stdRadius, stdRadius, stdRadius, stdRadius, stdRadius, stdRadius, stdRadius, stdRadius, cylinderRadius];
+    heights = [4, 10, 8, 4, cylinderHeight, 16, 4, 4, 18, 4, 4, 10, 6, 4, cylinderHeight];
+    orientations = [vertical, horizontal, vertical, vertical, vertical, horizontal, vertical, vertical, horizontal, vertical, vertical, horizontal, vertical, vertical, vertical];
+    var i = 0;
+    
+    for (; i < 15; i++) {
+        addCylinder(lowerMobile, xs[i], ys[i], 0, radius[i], radius[i], heights[i], orientations[i]);
+    }
+    for (; i < 18; i++) {
+        addCube(lowerMobile, xs[i], ys[i], 0, 2, false);
+    }
 
+    /*
     addCylinder(lowerMobile, 0, -2, 0, stdRadius, stdRadius, 4, vertical); // 3a
     addCylinder(lowerMobile, -3, -4, 0, stdRadius, stdRadius, 10, horizontal); // 3b
     addCylinder(lowerMobile, 2, -8, 0, stdRadius, stdRadius, 8, vertical); // 3c
@@ -48,34 +66,44 @@ function createLowerMobile(x, y, z) {
     addCylinder(lowerMobile, 6, -12, 0, stdRadius, stdRadius, 16, horizontal); // 3e
     addCylinder(lowerMobile, -2, -14, 0, stdRadius, stdRadius, 4, vertical); // 3f
     addCylinder(lowerMobile, 14, -14, 0, stdRadius, stdRadius, 4, vertical); // 3g
-    addCube(lowerMobile, -2, -17, 0, 2, false); // 8
     addCylinder(lowerMobile, 19, -16, 0, stdRadius, stdRadius, 18, horizontal); // 3h
     addCylinder(lowerMobile, 10, -18, 0, stdRadius, stdRadius, 4, vertical); // 3i
     addCylinder(lowerMobile, 28, -18, 0, stdRadius, stdRadius, 4, vertical); // 3j
-    addCube(lowerMobile, 28, -21, 0, 2, false); // 10
     addCylinder(lowerMobile, 9, -20, 0, stdRadius, stdRadius, 10, horizontal); // 3k
     addCylinder(lowerMobile, 4, -23, 0, stdRadius, stdRadius, 6, vertical); // 3l
     addCylinder(lowerMobile, 14, -22, 0, stdRadius, stdRadius, 4, vertical); // 3m
     addCylinder(lowerMobile, 4, -28, 0, cylinderRadius, cylinderRadius, cylinderHeight, vertical); // 9
+    addCube(lowerMobile, -2, -17, 0, 2, false); // 8
+    addCube(lowerMobile, 28, -21, 0, 2, false); // 10
     addCube(lowerMobile, 14, -25, 0, 2, false); // 11
+    */
+    
     scene.add(lowerMobile);
-
 
     lowerMobile.position.x = x;
     lowerMobile.position.y = y;
     lowerMobile.position.z = z;
 }
 
+function createMiddleMobile(x, y, z) {
+    'use strict';
+    middleMobile = new THREE.Object3D();
+    material = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: true });
+    createLowerMobile(scale * 12, scale * -34, scale * 0);
+    addCylinder(middleMobile, 0, -3, 0, stdRadius, stdRadius, 6, vertical); // 2a
+    addCylinder(middleMobile, -3, -6, 0, stdRadius, stdRadius, 14, horizontal); // 2b
+    scene.add(middleMobile);
+
+    middleMobile.position.x = x;
+    middleMobile.position.y = y;
+    middleMobile.position.z = z;
+}
+
 function createScene() {
     'use strict';
     scene = new THREE.Scene();
     scene.add(new THREE.AxisHelper(10));
-    createLowerMobile(0, 0, 0);
-    // createTable(0, 8, 0);
-    // createBall(0, 0, 15);
-    // createCylinder(0, 0, 0, 2, 2, 80, true); // (Xcoordinate, Ycoordinate, Zcoordinate, cylinderHeight);
-    // createCube(0, 0, 0, true);
-    // createBall(0, 0, 0); // (Xcoordinate, Ycoordinate, Zcoordinate, cylinderHeight);
+    createMiddleMobile(0, 0, 0);
 }
 
 function createCamera() {
@@ -102,6 +130,18 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
     switch (e.keyCode) {
+        case 37: // left arrow
+            leftArrow = true;
+            break;
+        case 38: // top arrow
+            topArrow = true;
+            break;
+        case 39: // right arrow
+            rightArrow = true;
+            break;
+        case 40: // down arrow
+            downArrow = true;
+            break;
         case 49: // 1 top
         case 97: // 1 side
             // camara frontal
@@ -122,27 +162,24 @@ function onKeyDown(e) {
                 }
             });
             break;
-        case 81: // Q/q
-            // rodar para a esquerda angulo v1
-            // cylinder.rotation.y += 0.05;
-            lowerMobile.userData.rotateY += lowerMobile.userData.step;
+        case 81: // Q/q --- rodar para a esquerda angulo v1
+            rotate_v1_l = true;
             break;
-        case 87: // W/w
-            // rodar para a direita angulo v1
-            lowerMobile.rotation.y -= 0.05;
+        case 87: // W/w -- rodar para a direita angulo v1
+            rotate_v1_r = true;
             break;
-        case 65: // A/a
-            // rodar para a esquerda angulo v2
-            lowerMobile.rotation.z += 0.05;
+        case 65: // A/a --- rodar para a esquerda angulo v2
+            rotate_v2_l = true;
             break;
-        case 67: // C/c
-            // rodar para a direita angulo v2
-            lowerMobile.rotation.z -= 0.05;
+        case 67: // C/c --- rodar para a direita angulo v2
+            rotate_v2_r = true;
             break;
-        case 68: // D/d
-            // rodar para a esquerda angulo v3
-        case 90: // Z/z
-            // rodar para a direita angulo v3
+        case 68: // D/d --- rodar para a esquerda angulo v3
+            rotate_v3_l = true;
+            break;
+        case 90: // Z/z --- rodar para a direita angulo v3
+            rotate_v3_r = true;
+            break;
         case 69:  // E
         case 101: // e
             scene.traverse(function (node) {
@@ -150,6 +187,42 @@ function onKeyDown(e) {
                     node.visible = !node.visible;
                 }
             });
+            break;
+    }
+}
+
+function onKeyUp(e) {
+    'use strict';
+    switch (e.keyCode) {
+        case 37: // left arrow
+            leftArrow = false;
+            break;
+        case 38: // top arrow
+            topArrow = false;
+            break;
+        case 39: // right arrow
+            rightArrow = false;
+            break;
+        case 40: // down arrow
+            downArrow = false;
+            break;
+        case 81: // Q/q --- rodar para a esquerda angulo v1
+            //rotate_v1_l = false;
+            break;
+        case 87: // W/w --- rodar para a direita angulo v1
+            //rotate_v1_r = false;
+            break;
+        case 65: // A/a --- rodar para a esquerda angulo v2
+            rotate_v2_l = false;
+            break;
+        case 67: // C/c --- rodar para a direita angulo v2
+            rotate_v2_r = false;
+            break;
+        case 68: // D/d --- rodar para a esquerda angulo v3
+            rotate_v3_l = false;
+            break;
+        case 90: // Z/z --- rodar para a direita angulo v3
+            rotate_v3_r = false;
             break;
     }
 }
@@ -171,17 +244,48 @@ function init() {
     render();
 
     window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
     window.addEventListener("resize", onResize);
 }
 
 function animate() {
     'use strict';
-    // if (ball.userData.jumping) {
-    //     ball.userData.step += 0.04;
-    //     ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
-    //     ball.position.z = 15 * (Math.cos(ball.userData.step));
-    // }
-    // if (lowerMobile.userData.)
+
+    if (leftArrow)
+        middleMobile.position.x--;
+    
+    if (topArrow)
+        middleMobile.position.z++;
+
+    if (rightArrow)
+        middleMobile.position.x++;
+
+    if (downArrow)
+        middleMobile.position.z--;
+
+    if (rotate_v1_r) {
+        upperMobile.rotation.y -= .03;
+        middleMobile.rotation.y -= .03;
+        lowerMobile.rotation.y -= .03;
+    }
+    else if (rotate_v1_l) {
+        upperMobile.rotation.y += .03;
+        middleMobile.rotation.y += .03;
+        lowerMobile.rotation.y += .03;
+    }
+    else if (rotate_v2_r) {
+        middleMobile.rotation.y -= .03;
+        lowerMobile.rotation.y -= .03;
+    }
+    else if (rotate_v2_l) {
+        middleMobile.rotation.y += .03;
+        lowerMobile.rotation.y += .03;
+    }
+    else if (rotate_v3_r)
+        lowerMobile.rotation.y -= .03;
+    else if (rotate_v3_l)
+        lowerMobile.rotation.y += .03;
+    
     render();
     requestAnimationFrame(animate);
 }
