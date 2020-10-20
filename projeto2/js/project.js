f011a4e3a62c8e460e08682e83d7ad6e9fe61fcc
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-10-20 18:44:11 +0100

    i draw a table

--- projeto2/js/project.js
+++ projeto2/js/project.js
@@ -1,4 +1,7 @@
-var scale = 5;
+var camera, scene, renderer;
+var scale = 2.5;
+var near = 1, far = 1500 * scale;
+var camera1, camera2, camera3;
 var ballRadius = 5 * scale;
 var stickLength = 80 * scale;
 var tableHoleRadius = 5 * scale;
@@ -9,20 +12,12 @@ var grey = new THREE.Color("rgb(150, 150, 150)");
 var ballMaterial = new THREE.MeshBasicMaterial({ color: pink, wireframe: true });
 var stickMaterial = new THREE.MeshBasicMaterial({ color: yellow, wireframe: true });
 var tableMaterial = new THREE.MeshBasicMaterial({ color: grey, wireframe: true});
+var planeMaterial = new THREE.MeshBasicMaterial( {color: grey, side: THREE.DoubleSide, wireframe: true} );
+var basePlane = new THREE.PlaneGeometry(tableWidth, tableDepth, 10);
+var longWallPlane = new THREE.PlaneGeometry(tableDepth, ballRadius * 2, 10, 10);
+var smallWallPlan = new THREE.PlaneGeometry(ballRadius * 2, tableWidth, 10, 10);
+var planeWidth = new THREE.PlaneGeometry(20 * scale, 5 * scale, 10);
 var cube = new THREE.BoxGeometry(tableWidth, ballRadius, tableDepth);
-//var table = new THREE.ExtrudeGeometry(cube, extrudeSettings);
-// var tableHoe
-
-var extrudeSettings = {
-	steps: 2,
-	depth: tableDepth - 5,
-	bevelEnabled: false
-	// bevelEnabled: true,
-	// bevelThickness: 1,
-	// bevelSize: 1,
-	// bevelOffset: 0,
-	// bevelSegments: 1
-};
 
 class Ball {
     constructor() {
@@ -69,22 +64,39 @@ class Stick {
                 this.angle = -Math.PI/2;
                 break;
         }
-
         this.obj.rotazeY(this.angle);
-        
     }
 }
 
 function createMesa() {
-    mesh = new THREE.Mesh(table, tableMaterial);
-    table = new THREE.Object3D();
-    table.add(mesh);
+    var base = new THREE.Mesh(basePlane, planeMaterial);
+    var leftWall = new THREE.Mesh(longWallPlane, planeMaterial);
+    var rightWall = new THREE.Mesh(longWallPlane, planeMaterial);
+    var topWall = new THREE.Mesh(smallWallPlan, planeMaterial);
+    var bottomWall = new THREE.Mesh(smallWallPlan, planeMaterial);
+    var table = new THREE.Object3D();
+    base.rotateX(Math.PI/2);
+    base.rotateZ(Math.PI/2);
+    table.add(base);
+    leftWall.position.set(0, ballRadius, tableWidth/2);
+    rightWall.position.set(0, ballRadius, -tableWidth/2);
+    topWall.position.set(-tableDepth/2, ballRadius, 0);
+    bottomWall.position.set(tableDepth/2, ballRadius, 0);
+    topWall.rotateX(Math.PI/2);
+    topWall.rotateY(Math.PI/2);
+    bottomWall.rotateX(Math.PI/2);
+    bottomWall.rotateY(Math.PI/2);
+    table.add(leftWall);
+    table.add(rightWall);
+    table.add(topWall);
+    table.add(bottomWall);
     scene.add(table);
 }
 
 function createScene() {
     'use strict';
     scene = new THREE.Scene();
+    scene.add(new THREE.AxisHelper(50));
     createMesa();
 }
 
@@ -94,8 +106,8 @@ function createCamera() {
     var height = window.innerHeight;
     camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, near, far);
     camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 200 * scale;
+    camera.position.y = 0 * scale;
+    camera.position.z = 200;
     camera.lookAt(scene.position);
 }
 
@@ -121,13 +133,115 @@ function init() {
 
     createScene();
     createCamera();
-
     render();
-    /*
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
-    */
     window.addEventListener("resize", onResize);
+}
+
+function onKeyDown(e) {
+    'use strict';
+    switch (e.keyCode) {
+        case 37: // left arrow
+            leftArrow = true;
+            break;
+        case 38: // top arrow
+            topArrow = true;
+            break;
+        case 39: // right arrow
+            rightArrow = true;
+            break;
+        case 40: // down arrow
+            downArrow = true;
+            break;
+        case 49: // 1 frontal view
+        case 97: // 1 frontal view
+            camera1 = true;
+            break;
+        case 50: // 2 top view
+        case 98: // 2 top view
+            camera2 = true;
+            break;
+        case 51: // 3 side view
+        case 99: // 3 side view
+            camera3 = true;
+            break;
+        case 52: // 4 alternate mesh
+        case 100: // 4 alternate mesh
+            material.wireframe = !material.wireframe;
+            yellowMaterial.wireframe = !yellowMaterial.wireframe;
+            orangeMaterial.wireframe = !orangeMaterial.wireframe;
+            greenMaterial.wireframe = !greenMaterial.wireframe;
+            pinkMaterial.wireframe = !pinkMaterial.wireframe;
+            blueMaterial.wireframe = !blueMaterial.wireframe;
+            break;
+        case 81: // Q/q --- rodar para a esquerda angulo v1
+            rotate_v1_l = true;
+            break;
+        case 87: // W/w -- rodar para a direita angulo v1
+            rotate_v1_r = true;
+            break;
+        case 65: // A/a --- rodar para a esquerda angulo v2
+            rotate_v2_l = true;
+            break;
+        case 67: // C/c --- rodar para a direita angulo v2
+            rotate_v2_r = true;
+            break;
+        case 68: // D/d --- rodar para a esquerda angulo v3
+            rotate_v3_l = true;
+            break;
+        case 90: // Z/z --- rodar para a direita angulo v3
+            rotate_v3_r = true;
+            break;
+    }
+}
+
+function onKeyUp(e) {
+    'use strict';
+    switch (e.keyCode) {
+        case 37: // left arrow
+            leftArrow = false;
+            break;
+        case 38: // top arrow
+            topArrow = false;
+            break;
+        case 39: // right arrow
+            rightArrow = false;
+            break;
+        case 40: // down arrow
+            downArrow = false;
+            break;
+        case 49: // 1 frontal view
+        case 97: // 1 frontal view
+            camera1 = false;
+            break;
+        case 50: // 2 top view
+        case 98: // 2 top view
+            camera2 = false;
+            break;
+        case 51: // 3 side view
+        case 99: // 3 side view
+            camera3 = false;
+            break;
+        case 81: // Q/q --- rodar para a esquerda angulo v1
+            rotate_v1_l = false;
+            break;
+        case 87: // W/w --- rodar para a direita angulo v1
+            rotate_v1_r = false;
+            break;
+        case 65: // A/a --- rodar para a esquerda angulo v2
+            rotate_v2_l = false;
+            break;
+        case 67: // C/c --- rodar para a direita angulo v2
+            rotate_v2_r = false;
+            break;
+        case 68: // D/d --- rodar para a esquerda angulo v3
+            rotate_v3_l = false;
+            break;
+        case 90: // Z/z --- rodar para a direita angulo v3
+            rotate_v3_r = false;
+            break;
+    }
 }
 
 function animate() {
@@ -144,10 +258,10 @@ function animate() {
 
     if (downArrow)
         g0.position.z--;
-
+    */
     if (camera1) {
-        camera.position.x = 0;
-        camera.position.y = 0;
+        camera.position.x = 200 * scale;
+        camera.position.y = 200 * scale;
         camera.position.z = 200 * scale;
     }
 
@@ -163,7 +277,7 @@ function animate() {
         camera.position.z = 0;
     }
 
-    
+    /*
     if (rotate_v1_r)
     g0.rotation.y -= .03;
     if (rotate_v1_l)
